b0bba055fc70ebaf4e01e2e547d74548a0ff40fd
Corey Innis <corey@coolerator.net>
2015-09-29 12:35:08 -0700

    Initial commit: "bundle gem -b --mit --test=rspec wip-checklist"

---
+++ lib/wip/checklist/version.rb
@@ -0,0 +1,5 @@
+module Wip
+  module Checklist
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/wip/checklist_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Wip::Checklist do
+  it 'has a version number' do
+    expect(Wip::Checklist::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
